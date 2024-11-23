62b0d68d21b68e8929186092c5b9d9ae0488eea0
j90137ze <zakaria.dandan@student.manchester.ac.uk>
2024-11-23 16:38:36 +0000

    Formatting Changes

--- Sin_Cos.py
+++ Sin_Cos.py
@@ -13,17 +13,18 @@ ax.set_xlim(0, 30)
 ax.set_ylim(-2, 2)  
 ax.set_zlim(-2, 2) 
 
+"""
 ax.set_facecolor('black')
 fig.patch.set_facecolor('black')
 ax.grid(False)
 ax.set_axis_off()
 
-fig.canvas.manager.set_window_title("Sin and Cos")
 
 ax.xaxis.pane.set_facecolor('black')
 ax.yaxis.pane.set_facecolor('black')
 ax.zaxis.pane.set_facecolor('black')
-
+"""
+fig.canvas.manager.set_window_title("Sin and Cos")
 
 dt = 0.1
 
@@ -33,9 +34,9 @@ x.append(0)
 y.append(math.sin(0))
 z.append(math.cos(0))
 
-line, = ax.plot(x, y, z, color = 'white')
+line, = ax.plot(x, y, z, color = 'Blue')
 
-while x[-1] < 100:
+while x[-1] < 30:
     x.append(x[-1] + dt)
     y.append(math.sin(x[-1]))
     z.append(math.cos(x[-1]))
